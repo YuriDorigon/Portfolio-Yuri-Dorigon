88e103928774be3c703c2ea411c24ad9bc7a28be
YuriDorigon <yuridorigon12@gmail.com>
2024-10-01 21:29:02 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function enviarMensagem() {
+    var nome = document.getElementById('nome').value;
+    var mensagem = document.getElementById('mensagem').value;
+
+    if (document.getElementById('nome').value === "" || document.getElementById('mensagem').value === "") {
+        Swal.fire({
+            toast: true,
+            position: "top-end",
+            icon: "error",
+            title: "Todos os campos devem ser preenchidos.",
+            showConfirmButton: false,
+            timer: 2000
+        });
+    } else{
+
+        var url = `https://wa.me/5548991136703?text=Ola,%20me%20chamo%20${nome}!!%0A${mensagem}`;
+        window.open(url, '_blank');
+    }
+
+
+    
+}
+
